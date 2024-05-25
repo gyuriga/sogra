d07fe48fc568743ce1f8056540f31e0f5f23a5c2
potato <65760583+juntae6942@users.noreply.github.com>
2024-05-25 19:43:04 +0900

    init

---
+++ back/src/main/java/CON/CON/backApplication.java
@@ -0,0 +1,13 @@
+package CON.CON;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class backApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(backApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/CON/CON/COnApplicationTests.java
@@ -0,0 +1,13 @@
+package CON.CON;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class COnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
